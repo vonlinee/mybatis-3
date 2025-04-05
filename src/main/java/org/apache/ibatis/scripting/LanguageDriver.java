d7d8362903d117ec08fd9aae6be734285622064c
vonlinee <vonlinee@163.com>
2025-04-05 23:52:20 +0800

    fix some typo

--- src/main/java/org/apache/ibatis/scripting/LanguageDriver.java
+++ src/main/java/org/apache/ibatis/scripting/LanguageDriver.java
@@ -27,7 +27,7 @@ import org.apache.ibatis.session.Configuration;
 public interface LanguageDriver {
 
   /**
-   * Creates a {@link ParameterHandler} that passes the actual parameters to the the JDBC statement.
+   * Creates a {@link ParameterHandler} that passes the actual parameters to the JDBC statement.
    *
    * @author Frank D. Martinez [mnesarco]
    *
@@ -46,12 +46,12 @@ public interface LanguageDriver {
 
   /**
    * Creates an {@link SqlSource} that will hold the statement read from a mapper xml file. It is called during startup,
-   * when the mapped statement is read from a class or an xml file.
+   * when the mapped statement is read from a class or a xml file.
    *
    * @param configuration
    *          The MyBatis configuration
    * @param script
-   *          XNode parsed from a XML file
+   *          XNode parsed from an XML file
    * @param parameterType
    *          input parameter type got from a mapper method or specified in the parameterType xml attribute. Can be
    *          null.
@@ -67,7 +67,7 @@ public interface LanguageDriver {
 
   /**
    * Creates an {@link SqlSource} that will hold the statement read from an annotation. It is called during startup,
-   * when the mapped statement is read from a class or an xml file.
+   * when the mapped statement is read from a class or a xml file.
    *
    * @param configuration
    *          The MyBatis configuration
