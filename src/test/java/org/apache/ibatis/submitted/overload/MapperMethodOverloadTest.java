546ed427aef3e90cf07f47f63c2bb9e0a89b43f3
vonlinee <vonlinee@163.com>
2025-04-19 00:15:23 +0800

    add test about mapper method overloading.

---
+++ src/test/java/org/apache/ibatis/submitted/overload/MapperMethodOverloadTest.java
@@ -0,0 +1,66 @@
+/*
+ *    Copyright 2009-2024 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.submitted.overload;
+
+import java.io.Reader;
+
+import org.apache.ibatis.BaseDataTest;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * method loading is not supported
+ */
+class MapperMethodOverloadTest {
+
+  private static SqlSessionFactory sqlSessionFactory;
+
+  @BeforeAll
+  static void setUp() throws Exception {
+    // create an SqlSessionFactory
+    try (Reader reader = Resources.getResourceAsReader("org/apache/ibatis/submitted/overload/mybatis-config.xml")) {
+      sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+    }
+
+    // populate in-memory database
+    BaseDataTest.runScript(sqlSessionFactory.getConfiguration().getEnvironment().getDataSource(),
+        "org/apache/ibatis/submitted/overload/CreateDB.sql");
+  }
+
+  @Test
+  void shouldGetAUser() {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      Mapper mapper = sqlSession.getMapper(Mapper.class);
+      // select * from users where id = ? and name = ?
+      User user = mapper.findUser(1);
+      Assertions.assertNull(user);
+
+      // select * from users where id = ? and name = ?
+      User user1 = mapper.findUser(1, "zs");
+      Assertions.assertNull(user1);
+
+      // select * from users where id = ?
+      User user2 = mapper.findUser(1, null);
+      Assertions.assertNotNull(user2);
+    }
+  }
+
+}
