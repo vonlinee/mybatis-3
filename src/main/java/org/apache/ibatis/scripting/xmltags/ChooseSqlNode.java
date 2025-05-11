6ffcf07956631f2aff4d291938330a2cfe83b699
vonlinee <vonlinee@163.com>
2025-05-11 09:11:12 +0800

    update license header

--- src/main/java/org/apache/ibatis/scripting/xmltags/ChooseSqlNode.java
+++ src/main/java/org/apache/ibatis/scripting/xmltags/ChooseSqlNode.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2009-2022 the original author or authors.
+ *    Copyright 2009-2025 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -41,5 +41,13 @@ public class ChooseSqlNode implements SqlNode {
       return true;
     }
     return false;
+  }
+
+  public SqlNode getDefaultSqlNode() {
+    return defaultSqlNode;
+  }
+
+  public List<SqlNode> getIfSqlNodes() {
+    return ifSqlNodes;
   }
 }
