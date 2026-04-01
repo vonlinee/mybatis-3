64ba439127108c2fb8b579c453ec4b7dfce398fe
vonlinee <vonlinee@163.com>
2026-04-02 00:11:25 +0800

    refactor: extract incomplete resolvers into separate class

--- src/main/java/org/apache/ibatis/session/Configuration.java
+++ src/main/java/org/apache/ibatis/session/Configuration.java
@@ -19,14 +19,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.BiFunction;
 
 import org.apache.ibatis.binding.CompositeMethodLookup;
 import org.apache.ibatis.binding.MappedStatementMethodLookup;
 import org.apache.ibatis.binding.MapperMethod;
 import org.apache.ibatis.binding.MapperRegistry;
-import org.apache.ibatis.builder.*;
+import org.apache.ibatis.builder.BuilderException;
+import org.apache.ibatis.builder.CacheRefResolver;
+import org.apache.ibatis.builder.IncompleteResolvers;
+import org.apache.ibatis.builder.ResultMapResolver;
 import org.apache.ibatis.builder.annotation.MethodResolver;
 import org.apache.ibatis.builder.annotation.SqlProviderFactory;
 import org.apache.ibatis.builder.xml.XMLMapperBuilder;
@@ -175,15 +177,7 @@ public class Configuration {
 
   protected final Set<String> loadedResources = new HashSet<>();
   protected final Map<String, XNode> sqlFragments = new StrictMap<>("XML fragments parsed from previous mappers");
-  protected final Collection<XMLStatementBuilder> incompleteStatements = new LinkedList<>();
-  protected final Collection<CacheRefResolver> incompleteCacheRefs = new LinkedList<>();
-  protected final Collection<ResultMapResolver> incompleteResultMaps = new LinkedList<>();
-  protected final Collection<MethodResolver> incompleteMethods = new LinkedList<>();
-
-  private final ReentrantLock incompleteResultMapsLock = new ReentrantLock();
-  private final ReentrantLock incompleteCacheRefsLock = new ReentrantLock();
-  private final ReentrantLock incompleteStatementsLock = new ReentrantLock();
-  private final ReentrantLock incompleteMethodsLock = new ReentrantLock();
+  protected final IncompleteResolvers incompleteResolvers = new IncompleteResolvers();
 
   /*
    * A map holds cache-ref relationship. The key is the namespace that references a cache bound to another namespace and
@@ -852,39 +846,19 @@ public class Configuration {
   }
 
   public void addIncompleteStatement(XMLStatementBuilder incompleteStatement) {
-    incompleteStatementsLock.lock();
-    try {
-      incompleteStatements.add(incompleteStatement);
-    } finally {
-      incompleteStatementsLock.unlock();
-    }
+    incompleteResolvers.addIncompleteStatement(incompleteStatement);
   }
 
   public void addIncompleteCacheRef(CacheRefResolver incompleteCacheRef) {
-    incompleteCacheRefsLock.lock();
-    try {
-      incompleteCacheRefs.add(incompleteCacheRef);
-    } finally {
-      incompleteCacheRefsLock.unlock();
-    }
+    incompleteResolvers.addIncompleteCacheRef(incompleteCacheRef);
   }
 
   public void addIncompleteResultMap(ResultMapResolver resultMapResolver) {
-    incompleteResultMapsLock.lock();
-    try {
-      incompleteResultMaps.add(resultMapResolver);
-    } finally {
-      incompleteResultMapsLock.unlock();
-    }
+    incompleteResolvers.addIncompleteResultMap(resultMapResolver);
   }
 
   public void addIncompleteMethod(MethodResolver builder) {
-    incompleteMethodsLock.lock();
-    try {
-      incompleteMethods.add(builder);
-    } finally {
-      incompleteMethodsLock.unlock();
-    }
+    incompleteResolvers.addIncompleteMethod(builder);
   }
 
   public MappedStatement getMappedStatement(String id) {
@@ -962,87 +936,19 @@ public class Configuration {
   }
 
   public void parsePendingMethods(boolean reportUnresolved) {
-    if (incompleteMethods.isEmpty()) {
-      return;
-    }
-    incompleteMethodsLock.lock();
-    try {
-      incompleteMethods.removeIf(x -> {
-        x.resolve();
-        return true;
-      });
-    } catch (IncompleteElementException e) {
-      if (reportUnresolved) {
-        throw e;
-      }
-    } finally {
-      incompleteMethodsLock.unlock();
-    }
+    incompleteResolvers.parsePendingMethods(reportUnresolved);
   }
 
   public void parsePendingStatements(boolean reportUnresolved) {
-    if (incompleteStatements.isEmpty()) {
-      return;
-    }
-    incompleteStatementsLock.lock();
-    try {
-      incompleteStatements.removeIf(x -> {
-        x.parseStatementNode();
-        return true;
-      });
-    } catch (IncompleteElementException e) {
-      if (reportUnresolved) {
-        throw e;
-      }
-    } finally {
-      incompleteStatementsLock.unlock();
-    }
+    incompleteResolvers.parsePendingStatements(reportUnresolved);
   }
 
   public void parsePendingCacheRefs(boolean reportUnresolved) {
-    if (incompleteCacheRefs.isEmpty()) {
-      return;
-    }
-    incompleteCacheRefsLock.lock();
-    try {
-      incompleteCacheRefs.removeIf(x -> x.resolveCacheRef() != null);
-    } catch (IncompleteElementException e) {
-      if (reportUnresolved) {
-        throw e;
-      }
-    } finally {
-      incompleteCacheRefsLock.unlock();
-    }
+    incompleteResolvers.parsePendingCacheRefs(reportUnresolved);
   }
 
   public void parsePendingResultMaps(boolean reportUnresolved) {
-    if (incompleteResultMaps.isEmpty()) {
-      return;
-    }
-    incompleteResultMapsLock.lock();
-    try {
-      boolean resolved;
-      IncompleteElementException ex = null;
-      do {
-        resolved = false;
-        Iterator<ResultMapResolver> iterator = incompleteResultMaps.iterator();
-        while (iterator.hasNext()) {
-          try {
-            iterator.next().resolve();
-            iterator.remove();
-            resolved = true;
-          } catch (IncompleteElementException e) {
-            ex = e;
-          }
-        }
-      } while (resolved);
-      if (reportUnresolved && !incompleteResultMaps.isEmpty() && ex != null) {
-        // At least one result map is unresolvable.
-        throw ex;
-      }
-    } finally {
-      incompleteResultMapsLock.unlock();
-    }
+    incompleteResolvers.parsePendingResultMaps(reportUnresolved);
   }
 
   /**
