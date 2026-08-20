676b6fe32caa33f37a6b7ff0cb09dcafca2a2d10
vonlinee <vonlinee@163.com>
2026-08-21 00:20:19 +0800

    refactor(xml): simplify XMLMapperBuilder parsing calls

--- src/main/java/org/apache/ibatis/builder/xml/XMLConfigBuilder.java
+++ src/main/java/org/apache/ibatis/builder/xml/XMLConfigBuilder.java
@@ -400,16 +400,12 @@ public class XMLConfigBuilder extends BaseBuilder {
         if (resource != null && url == null && mapperClass == null) {
           ErrorContext.instance().resource(resource);
           try (InputStream inputStream = Resources.getResourceAsStream(resource)) {
-            XMLMapperBuilder mapperParser = new XMLMapperBuilder(inputStream, configuration, resource,
-                configuration.getSqlFragments());
-            mapperParser.parse();
+            XMLMapperBuilder.parse(configuration, resource, inputStream);
           }
         } else if (resource == null && url != null && mapperClass == null) {
           ErrorContext.instance().resource(url);
           try (InputStream inputStream = Resources.getUrlAsStream(url)) {
-            XMLMapperBuilder mapperParser = new XMLMapperBuilder(inputStream, configuration, url,
-                configuration.getSqlFragments());
-            mapperParser.parse();
+            XMLMapperBuilder.parse(configuration, url, inputStream);
           }
         } else if (resource == null && url == null && mapperClass != null) {
           Class<?> mapperInterface = Resources.classForName(mapperClass);
